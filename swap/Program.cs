70c25a4c7e1562f9369d700f316e1dc650526cdb
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-22 21:08:49 +0300

    swap, switch

---
+++ swap/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+void Swap(ref int a, ref int b)
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void Calkulator (int a, int b, ref int sum, ref int multi)
+{
+    sum=a+b;
+    multi=a*b;
+}
+
+int a=3, b=5;
+
+System.Console.WriteLine($"a={a} b={b}");
+Swap (ref a, ref b);
+System.Console.WriteLine($"a={a} b={b}");
+int sum=0, multi=0;
+Calkulator (a,b, out sum, out multi);
---
+++ switch/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводится число. Определить день недели
+
+int day=2;
+
+switch(day)
+{
+    case 1:
+    System.Console.WriteLine("Понедельник");
+    break;
+    case 2:
+    System.Console.WriteLine("Вторник");
+    break;
+    default:
+    System.Console.WriteLine("неверное число");
+    break;
+}
