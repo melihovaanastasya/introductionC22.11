05eeb93891180c71ce01b7bbdfd94d2b5fd0c270
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-22 22:58:29 +0300

    32

--- 32/Program.cs
+++ 32/Program.cs
@@ -1,1 +1,19 @@
 ﻿// 32. Задать массив из 8 целых элементов и вывести их на экран
+
+int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int [] a=new int [size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+    a[i]=random.Next(min,max);
+    return a;
+}
+
+void Print(int [] a)
+{
+    for (int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],5}");
+}
+
+int [] a=RandomIntArray(8,0,100);
+Print(a);
